7b2e90ab3b6807eab620d09adcde485db2e728e4
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-11-03 21:43:37 +0200

    fix bug

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,3 @@
-import os
-
 from dotenv import find_dotenv, load_dotenv
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -56,12 +54,9 @@ class TestingConfig(BaseModel):
 
 
 class Settings(BaseModel):
-    db: DatabaseConfig = DatabaseConfig()  # type: ignore
+    db: DatabaseConfig = DatabaseConfig()
     test_db: TestingConfig = TestingConfig()
     logging: str = "INFO"
 
 
 settings = Settings()
-
-if __name__ == "__main__":
-    print(settings.db.url)
